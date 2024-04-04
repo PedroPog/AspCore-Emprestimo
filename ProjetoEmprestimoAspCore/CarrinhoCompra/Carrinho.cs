5dccf28f45b46fc34b5a07ec163ee7ea3d2d8e00
PedroPog <pevieiraf@gmail.com>
2024-04-03 22:21:16 -0300

    Cookie

---
+++ ProjetoEmprestimoAspCore/CarrinhoCompra/Carrinho.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Newtonsoft.Json;
+using ProjetoEmprestimoAspCore.Models;
+
+namespace ProjetoEmprestimoAspCore.CarrinhoCompra
+{
+    public class Carrinho
+    {
+        private string Key = "COOKIE.COMPRAS";
+        private Cookie.Cookie _cookie;
+
+        public Carrinho(Cookie.Cookie cookie)
+        {
+            _cookie = cookie;
+            
+        }
+        public void Salvar(List<Livro> list)
+        {
+            string Valor = JsonConvert.SerializeObject(list);
+            _cookie.Cadastrar(Key, Valor);
+        }
+        public List<Livro> Consultar()
+        {
+            return 
+        }
+    }
+}
---
+++ ProjetoEmprestimoAspCore/Cookie/Cookie.cs
@@ -0,0 +1,50 @@
+﻿namespace ProjetoEmprestimoAspCore.Cookie
+{
+    public class Cookie
+    {
+        private IHttpContextAccessor _context;
+        private IConfiguration _configuration;
+
+        public Cookie(IHttpContextAccessor context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        public void Cadastrar(string key, string valor)
+        {
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTime.Now.AddDays(7);
+            options.IsEssential = true;
+
+            _context.HttpContext.Response.Cookies.Append(key, valor,options);
+        }
+
+        public void Atualizar(string key, string valor)
+        {
+            if (Existe(key))
+            {
+                Remover(key);
+            }
+            Cadastrar(key, valor);
+        }
+
+        public void Remover(string key)
+        {
+            _context.HttpContext.Response.Cookies.Delete(key);
+        }
+        public string Consultar(string key,bool Cript = true)
+        {
+            var valor = _context.HttpContext.Request.Cookies[key];
+            return valor;
+        }
+        public bool Existe(string key)
+        {
+            if (_context.HttpContext.Request.Cookies[key]== null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
